86e67f134c74493ba01340cb1472b3c1d6238a18
Jess Guttenberg <jessigu@gmail.com>
2018-12-28 02:26:30 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,18 +1,13 @@
 def turn_count(board)
   counter = 0
   board.each do |turn|
-<<<<<<< HEAD
-    if turn == "X" || turn == "O"
-=======
     if board[turn] == "X" || board[turn] == "O"
->>>>>>> a64c62ad6ba69017b5fdd2e0e1e8fec965b309dd
       counter += 1
     end
   end
   return counter
 end
 
-<<<<<<< HEAD
 =begin
 def current_player(board)
   if turn_count(board) % 2 == 0
@@ -25,7 +20,4 @@ end
 
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
-=======
-def current_player
->>>>>>> a64c62ad6ba69017b5fdd2e0e1e8fec965b309dd
 end
